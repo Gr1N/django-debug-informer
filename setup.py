a911c357070fbb5a225a60e3db3aee7d80731047
Nikita Grishko <grin.minsk@gmail.com>
2015-03-08 18:46:10 +0300

    Preparing release 0.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-debug-informer',
-    version='0.0.1',
+    version='0.1.0',
     description='A simple application that helps displays various debug information about the Django project',
     long_description=open('README.md', encoding='utf-8').read(),
     author='Nikita Grishko',
